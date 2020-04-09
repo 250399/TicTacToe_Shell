e8a54dc76122c31f744153cd65fba122ccb8cae8
Kunal <kunaldeshmukh2503@gmail.com>
2020-04-09 06:20:57 -0700

    if computer can win then play that move

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -48,6 +48,98 @@ checkWinner () {
 	fi
 }
 
+checkTwo () {
+	if [ "$flag" = "player" ]
+	then
+		return 
+	fi
+	index1=$1
+	index2=$2
+	index3=$3
+	checkCompWin=$4
+	if [ "${board[$index1]}" = "${board[$index2]}" -a "${board[$index1]}" != "-" -a "${board[$index3]}" = "-" -a "${board[$index1]}" = "$checkCompWin" ] 
+	then
+		board[$index3]=$comp
+	 	flag=player
+		remMoves=$((remMoves-1))
+		printBoard
+	fi
+	if [ "${board[$index1]}" = "${board[$index3]}" -a "${board[$index1]}" != "-" -a "${board[$index2]}" = "-"  -a "${board[$index1]}" = "$checkCompWin" ]
+	then
+	 	board[$index2]=$comp
+		remMoves=$((remMoves-1))
+	 	flag=player
+		printBoard
+	fi
+	if [ "${board[$index3]}" = "${board[$index2]}" -a "${board[$index3]}" != "-" -a "${board[$index1]}" = "-"  -a "${board[$index2]}" = "$checkCompWin" ]
+	then
+		board[$index1]=$comp
+		flag=player
+		printBoard
+		remMoves=$((remMoves-1))
+	fi
+
+}
+
+randomPlay () {
+	pos=$((RANDOM%9))
+	if [ "${board[$pos]}" = "-" ] 
+	then 
+		board[$pos]=$comp 
+		remMoves=$((remMoves-1))
+	else
+		randomPlay
+	fi
+	flag=player
+	printBoard
+
+}
+
+winOrBlock () {
+	if [ "$flag" = "player" ]
+	then
+		return 
+	fi
+	index1=$1
+	index2=$2
+	index3=$3
+	if [ "${board[$index1]}" = "${board[$index2]}" -a "${board[$index1]}" != "-" -a "${board[$index3]}" = "-" ] 
+	then
+		board[$index3]=$comp
+	 	flag=player
+		remMoves=$((remMoves-1))
+		printBoard
+	fi
+	if [ "${board[$index1]}" = "${board[$index3]}" -a "${board[$index1]}" != "-" -a "${board[$index2]}" = "-" ]
+	then
+	 	board[$index2]=$comp
+		remMoves=$((remMoves-1))
+	 	flag=player
+		printBoard
+	fi
+	if [ "${board[$index3]}" = "${board[$index2]}" -a "${board[$index3]}" != "-" -a "${board[$index1]}" = "-" ]
+	then
+		board[$index1]=$comp
+		flag=player
+		printBoard
+		remMoves=$((remMoves-1))
+	fi
+
+}
+
+winOrBlock () {
+	letter=$1
+	checkTwo 0 1 2 $letter
+	checkTwo 3 4 5 $letter
+	checkTwo 6 7 8 $letter
+	checkTwo 0 4 8 $letter
+	checkTwo 2 4 6 $letter
+	checkTwo 0 3 6 $letter
+	checkTwo 1 4 7 $letter
+	checkTwo 2 5 8 $letter
+	[ "$flag" = "comp" ] && randomPlay || :
+}
+
 printBoard () {
 	for i in {0..8}
 	do
@@ -71,20 +163,15 @@ play () {
 		play
 	fi
 	flag=comp
+	elif [ $difficulty -eq 2 -a "$flag" = "comp" ]
+	then
+		winOrBlock $comp
 	else
-	pos=$((RANDOM%9))
-	if [ "${board[$pos]}" = "-" ] 
-	then 
-		board[$pos]=$comp 
-		remMoves=$((remMoves-1))
-	else
-		play
-		remMoves=$((remMoves-1))
-	fi 
-	flag=player
+		randomPlay
 	fi
-	printBoard
 }
+
+read -p"Enter difficulty 1.Easy 2.Hard" difficulty 
 flag=player
 remMoves=9
 reset
