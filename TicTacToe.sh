f008b1e1404f3c9f6e7d36e071a95a7cee219410
Kunal <kunaldeshmukh2503@gmail.com>
2020-04-09 04:59:10 -0700

    Player begins with toss to check who plays first

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -4,6 +4,47 @@ X#!/bin/bash
 reset () {
 	echo Welcome to TicTacToe Program!
 	arr=(- - - - - - - - -)
+	firstPlay
 }
 
+firstPlay () {
+	read -p"Enter 1 or 0 for a toss " toss
+	[ $toss -eq 1 -o $toss -eq 0 ] && : || reset
+	if [ $((RANDOM%2)) -eq $toss ]
+	then
+		echo You won the toss
+		read -p"Please choose X or O" player
+		player=${player^^}
+		if [ "$player" = "X" ]
+		then
+			comp=O
+			flag=player 
+		elif [ "$player" = "O" ]
+		then
+			comp=X
+			flag=comp
+		else
+			echo invalid choice
+			reset
+			return
+		fi
+	else
+		echo You lost the toss
+		if [ $((RANDOM%2)) -eq 1 ]
+		then
+			comp=X
+			player=O
+			flag=com
+		else
+			comp=O
+			player=X
+			flag=player
+		fi
+	fi
+	echo "The comp is "$comp
+	echo "You are "$player
+}
+
+
 reset
+
